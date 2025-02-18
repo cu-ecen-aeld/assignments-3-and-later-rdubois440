b52513779bd35a22c8a7d0353c7e46993fc5b1c9
rdubois440 <rdubois440@protonmail.com>
2025-02-18 14:15:10 +0100

    Sunny day

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,6 +15,32 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+	usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+	int retval = pthread_mutex_lock(thread_func_args->lock);
+	if (retval != 0) {
+		printf("Could not lock mutex %d %s\n", retval, strerror(retval));
+		thread_func_args->thread_complete_success = false;
+    	return thread_param;
+	}
+	printf("try lock succeeded, mutex was not locked\n");	
+
+
+	thread_func_args->thread_complete_success = true;
+
+	usleep(thread_func_args->wait_to_release_ms * 1000);
+
+
+	retval = pthread_mutex_unlock(thread_func_args->lock);
+	if (retval != 0) {
+		printf("Could not unlock mutex %d %s\n", retval, strerror(retval));
+		thread_func_args->thread_complete_success = false;
+    	return thread_param;
+	}
+
     return thread_param;
 }
 
@@ -28,6 +55,29 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+	struct thread_data *thData = malloc(sizeof *thData);
+
+    if (thData == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+
+	thData->wait_to_obtain_ms = wait_to_obtain_ms;
+	thData->wait_to_release_ms = wait_to_release_ms;
+	thData->lock = mutex;
+
+
+	int retval = pthread_create(thread, NULL, &threadfunc, thData);
+	if (retval != 0) {
+            printf("can't create thread :[%s]\n", strerror(retval));
+			thData->thread_complete_success = false;
+			return false;
+		}
+
+
+    printf("After Thread\n");
+	thData->thread_complete_success = true;
+    return true;
 }
 
